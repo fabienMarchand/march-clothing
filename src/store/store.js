e9b452f20fcd2209f5d3d86060b0ce4a8c9737a7
Marcha <marchaf@gmail.com>
2022-10-13 13:18:07 +0200

    bug fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,30 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from 'redux-persist/lib/storage';
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV === "develepoment" && logger].filter(
+  Boolean
+);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-//define what will be persistant or not 
+//define what will be persistant or not
 const persistConfig = {
-    key: 'root',
-    storage,
-    blacklist: ['user']
+  key: "root",
+  storage,
+  blacklist: ["user"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // root-reducer
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
 
 export const persistor = persistStore(store);

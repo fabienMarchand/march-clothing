19c239c12e5a9cb7d531f7ecf40c2583037a65d2
Marcha <marchaf@gmail.com>
2022-10-13 13:26:01 +0200

    add devtools

--- src/store/store.js
+++ src/store/store.js
@@ -9,7 +9,13 @@ const middleWares = [process.env.NODE_ENV === "develepoment" && logger].filter(
   Boolean
 );
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 //define what will be persistant or not
 const persistConfig = {

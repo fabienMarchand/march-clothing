73b0863ecb824ec3d162645a5f6c7f032ca3fbd3
Marcha <marchaf@gmail.com>
2022-10-20 15:49:09 +0200

    fix state issue

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
+import { compose, createStore, applyMiddleware, Middleware } from "redux";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 // import thunk from "redux-thunk";
@@ -9,12 +9,33 @@ import { rootSaga } from "./root-saga";
 
 import { rootReducer } from "./root-reducer";
 
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+//define what will be persistant or not
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
 const sagaMiddleware = createSagaMiddleware();
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
   sagaMiddleware,
-].filter(Boolean);
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
@@ -23,15 +44,6 @@ const composeEnhancer =
   compose;
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-//define what will be persistant or not
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["cart"],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // root-reducer
 export const store = createStore(
